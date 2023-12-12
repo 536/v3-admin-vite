b2a88c32a809d2520e23c17887677ab22d19f1c3
536 <944705050@qq.com>
2023-12-12 18:27:18 +0000

    Deploying to gh-pages from  @ 6fe55bd224c4f336203c010a976a3e92f4ff79e9 🚀

---
+++ static/index-jI7fUnpc.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-RjJjRl2O.js";import{ag as e,p as c,T as o,O as a,m as s}from"./vue-iKzacZb4.js";import"./element-FGu17exM.js";const _={},i={class:"app-container"};function m(p,d){const t=e("router-view"),r=e("el-card");return s(),c("div",i,[o(r,{header:"三级路由 - menu1-2"},{default:a(()=>[o(t)]),_:1})])}const x=n(_,[["render",m]]);export{x as default};
